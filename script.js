7444447139921375bdf31abd0ed0ad249bc2656b
Andrew Chou <andrewchou1996@gmail.com>
2022-11-18 11:18:51 -0800

    Arrays declaration

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Assignment Code
+// {========= Assignment Code ==========} 
 var generateBtn = document.querySelector("#generate");
 
+// {========= Notes ==========} 
 // This page says
 // Enter disired password length (between 8~128 characters): 
 // Include lowercase letters?
@@ -8,21 +9,40 @@ var generateBtn = document.querySelector("#generate");
 // Inculde numeric values?
 // Include specail characters?
 
-// This function will generate password
+// {========= This function will generate password ==========} 
 function generatePassword() {
-  // Declare arrarys for the password value: lowerCase, upperCase, num, spcial, empty(for the result)
+  // <---------- Declare arrarys for the password value: lowerCase, upperCase, num, spcial, empty(for the result) ----------> 
+  // upper case array
+  const upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  // Prompt the message and store the value for the length of the generatred password
+  // lower case array
+  const lowerCaseChars = [];
+  upperCaseChars.forEach(char => { lowerCaseChars.push(char.toLowerCase()) });
 
-  // Use window confirm() to check a series of criteria
+  // numeric value from 0~9
+  const numArray = [];
+  for (let index = 0; index <= 9; index++) {
+    numArray.push(index);
+  }
 
-  // Condition check and actual implementation: condition check, random output, etc.
+  // special characters
+  const specialChars = ["!", "@", "#", "$", "%", "&", "*", "?"];
 
-  // Finally, return the result
+  // <---------- Prompt the message and store the value for the length of the generatred password ----------> 
+
+  // <---------- Use window confirm() to check a series of criteria ----------> 
+
+  // <---------- Condition check and actual implementation: condition check, random output, etc. ----------> 
+
+  // <---------- Finally, return the result ----------> 
+  console.log(upperCaseChars);
+  console.log(lowerCaseChars);
+  console.log(numArray);
+  console.log(specialChars);
 }
 
 
-// Write password to the #password input
+// {========= Write password to the #password input =========} 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -30,7 +50,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// {========= Add event listener to generate button =========} 
 generateBtn.addEventListener("click", writePassword);  // call back writePassword method
 
 
